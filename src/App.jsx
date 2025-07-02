6babf1bda3cce84b3949997d17af5a4d70d3ca29
Adithya S Sekhar <adithya.s@edstem.com>
2025-07-02 11:58:40 +0530

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs"
 function App() {
   return (
-    <div>
-      "Hi"
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <Tabs defaultValue="modules" className="">
+        <TabsList>
+          <TabsTrigger value="modules">modules</TabsTrigger>
+          <TabsTrigger value="organization">organization</TabsTrigger>
+          <TabsTrigger value="risktype">risktype</TabsTrigger>
+        </TabsList>
+        <TabsContent value="modules">Modules</TabsContent>
+        <TabsContent value="organization">organization</TabsContent>
+        <TabsContent value="risktype">risktype</TabsContent>
+      </Tabs>
     </div>
   )
 }
